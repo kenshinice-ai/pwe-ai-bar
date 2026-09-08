c735e6156a35ff54cc4b52466d55271f54a4ad19
Lee L <lee.liu.melbourne@gmail.com>
2026-09-08 18:26:57 +1000

    Release 1.0.9

--- Casks/pwe-ai-bar.rb
+++ Casks/pwe-ai-bar.rb
@@ -6,8 +6,8 @@
 # this file should not be edited by hand for a release.
 
 cask "pwe-ai-bar" do
-  version "1.0.8"
-  sha256 "5f9cb584ba478b63546c9cec849c3fbc692987d76aa5df0ae5912e04059e9b14"
+  version "1.0.9"
+  sha256 "ff565c7afe75a650f9539f2ffaa148f5f4586673c3575f4cc6a39c5eab964dcb"
 
   url "https://github.com/kenshinice-ai/pwe-ai-bar/releases/download/v#{version}/PWE-AI-Bar-#{version}.dmg"
   name "PWE AI Bar"
